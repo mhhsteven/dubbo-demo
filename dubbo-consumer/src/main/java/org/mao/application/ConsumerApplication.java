95edc8aa346e80c8a468ed8e4704c9e64b9cedef
mhh <mhh@mogoroom.com>
2019-03-22 15:31:26 +0800

    dubbo2.7配置

--- dubbo-consumer/src/main/java/org/mao/application/ConsumerApplication.java
+++ dubbo-consumer/src/main/java/org/mao/application/ConsumerApplication.java
@@ -1,28 +1,39 @@
 package org.mao.application;
 
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.dubbo.config.annotation.Reference;
 import org.mao.dubbo.api.IFly;
 import org.mao.dubbo.dto.FlyDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
+@EnableAutoConfiguration
 public class ConsumerApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerApplication.class);
 
+    @Reference
+    private IFly fly;
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            FlyDTO flyDTO = new FlyDTO();
+            flyDTO.setHow(1L);
+            flyDTO.setWhen(new Date());
+            flyDTO.setWho("me");
+            Boolean canFly = fly.canFly(flyDTO);
+            LOGGER.info("fly.canFly result: {}", canFly);
+        };
+    }
+
     public static void main(String[] args) throws Exception {
-        PropertyConfigurator.configure("dubbo-demo/dubbo-consumer/src/main/resources/log4j.properties");
-        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
-        IFly fly = applicationContext.getBean(IFly.class);
-        FlyDTO flyDTO = new FlyDTO();
-        flyDTO.setHow(1L);
-        flyDTO.setWhen(new Date());
-        flyDTO.setWho("you");
-        Boolean canFly = fly.canFly(flyDTO);
-        LOGGER.info("fly.canFly result: {}", canFly);
+        SpringApplication.run(ConsumerApplication.class, args);
+        System.exit(0);
     }
 }
