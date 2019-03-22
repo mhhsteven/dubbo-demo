54f1e007c2bf18b1e150dc17922c8da5a4d66c23
mhh <mhh@mogoroom.com>
2019-03-22 15:18:40 +0800

    dubbo2.7配置

--- dubbo-provider/src/main/java/org/mao/application/ProviderApplication.java
+++ dubbo-provider/src/main/java/org/mao/application/ProviderApplication.java
@@ -1,18 +1,12 @@
 package org.mao.application;
 
-import org.apache.log4j.PropertyConfigurator;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-import java.util.concurrent.CountDownLatch;
-
+@EnableAutoConfiguration
 public class ProviderApplication {
 
-    private static CountDownLatch countDownLatch = new CountDownLatch(1);
-
-    public static void main(String[] args) throws Exception {
-        PropertyConfigurator.configure("dubbo-demo/dubbo-provider/src/main/resources/log4j.properties");
-        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
-        countDownLatch.await();
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
--- dubbo-provider/src/main/java/org/mao/dubbo/impl/FlyImpl.java
+++ dubbo-provider/src/main/java/org/mao/dubbo/impl/FlyImpl.java
@@ -1,10 +1,10 @@
 package org.mao.dubbo.impl;
 
+import org.apache.dubbo.config.annotation.Service;
 import org.mao.dubbo.api.IFly;
 import org.mao.dubbo.dto.FlyDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 @Service
 public class FlyImpl implements IFly {
